1c8890e97e85481ea4a942c972c22b19e1c24531
Rodrigo Valente <valentergs@gmail.com>
2020-10-08 14:08:18 -0300

    custom err string on Error()

--- main.go
+++ main.go
@@ -1,52 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"os"
 )
 
+type errorString string
+
+// Aqui a funçao Error() sofre um by-pass atraves de uma string que aramazena a mensagem para imprimir um erro customizado.
+func (es errorString) Error() string {
+	return fmt.Sprintf("Aqui está o erro: %s", string(es))
+}
+
 func main() {
-	f, err := os.Open("somefile.txt")
-
-	// O jeito mas comum de tratar um erro é iniciar um panic()
-	//panic(err)
-
-	// O jeito recomendado é utilizar fmt.Errorf() e errors.Is, que compara erros.
-	// if errors.Is(err, os.ErrPermission) {
-	// 	err = fmt.Errorf("você não tem permissão para abrir arquivo: %w", err)
-	// 	log.Println(err)
-	// } else if errors.Is(err, os.ErrNotExist) {
-	// 	err = fmt.Errorf("arquivo não existe %w", err)
-	// 	log.Println(err)
-	// }
-
-	var pathError *os.PathError
-
-	// // errors.As assinala ao "Target", nesse caso *os.PathError, o valor do erro pretendido.
-	// if errors.As(err, &pathError) {
-	// 	// return fmt.Errorf("Arquivo : %w e error path %s", err, pathError.Path)
-	// 	fmt.Printf("Error: %s\nPath: %s\nPathOp: %s\nPathErr: %s\n", err, pathError.Path, pathError.Op, pathError.Err)
-	// } else {
-	// 	fmt.Println(err)
-	// }
-
-	switch {
-	case errors.Is(err, os.ErrPermission):
-		err = fmt.Errorf("você não tem permissão para abrir arquivo: %w", err)
-		log.Println(err)
-	case errors.Is(err, os.ErrNotExist):
-		err = fmt.Errorf("arquivo não existe %w", err)
-		log.Println(err)
-	case errors.As(err, &pathError):
-		fmt.Printf("Error: %s\nPath: %s\nPathOp: %s\nPathErr: %s\n", err, pathError.Path, pathError.Op, pathError.Err)
-	case err != nil:
-		fmt.Println(err)
+	n, err := soma(1, 1)
+	if err != nil {
+		log.Fatalln(err)
 	}
+	fmt.Println(n)
+}
 
-	defer f.Close()
-
-	//fmt.Println(err)
-
+func soma(i, j int) (int, error) {
+	n := i * j
+	if n != i+j {
+		// Aqui é possivel assignar uma mensagem ao esso do tipo errorString
+		var sErr errorString = "a soma não funcionou"
+		log.Fatalln(0, sErr)
+	}
+	return n, nil
 }
