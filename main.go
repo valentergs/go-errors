33a340b0340f7b087dc7ca082e627c30df8f9ee4
Rodrigo Valente <valentergs@gmail.com>
2020-10-08 15:34:06 -0300

    error stacking

--- main.go
+++ main.go
@@ -2,35 +2,59 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 )
 
-type errorString string
-
-// Aqui a funçao Error() sofre um by-pass atraves de uma string que aramazena a mensagem para imprimir um erro customizado.
-func (es errorString) Error() string {
-	return fmt.Sprintf("Aqui está o erro: %s", string(es))
-}
-
 func main() {
-	n, err := soma(1, 1)
+	/*
+		fOne, err := os.Open("fileOne.txt")
+		if err != nil {
+			fmt.Errorf("Não consegui abrir o arquivo: %w", err)
+		}
+		fOne.Close()
+
+		fTwo, err := os.Create("fileTwo.txt")
+		if err != nil {
+			fmt.Errorf("Não consegui criar o arquivo: %w", err)
+		}
+		fTwo.Close()
+
+		if _, err := io.Copy(fTwo, fOne); err != nil {
+			fmt.Errorf("não consegui copiar: %w", err)
+		}
+	*/
+
+	err := copiarArq("arq2.txt", "arq1.txt")
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln("erro ao exec copiarArq():", err)
 	}
-	fmt.Println(n)
 }
 
-func soma(i, j int) (int, error) {
-	n := i * j
-	if n != i+j {
-		// Aqui é possivel assignar uma mensagem ao esso do tipo errorString
-		var sErr errorString = "a soma não funcionou"
-		log.Fatalln(0, sErr)
-	}
-	return n, nil
-}
+func copiarArq(destino, origem string) error {
 
-/*
-Devemos SEMPRE adicionar informação ao erro, não importa quão pequena seja a função
-O jeito mais fácil que adicionar informação a um erro é usando fmt.Errorf("%w", err). O exemplo acima é o "mais trabalhoso".
-*/
+	// Abrir o arquivo de origem
+	fOne, err := os.Open(origem)
+	if err != nil {
+		return fmt.Errorf("Não consegui abrir o arquivo: %w", err)
+	}
+	defer fOne.Close()
+
+	// Criar e abrir arquivo de destino
+	fTwo, err := os.Create(destino)
+	if err != nil {
+		return fmt.Errorf("Não consegui criar o arquivo: %w", err)
+	}
+	defer fTwo.Close()
+
+	// Copiar arquivo no arquivo de destino o arquivo de origem
+	i, err := io.Copy(fTwo, fOne)
+	if err != nil {
+		return fmt.Errorf("não consegui copiar: %w", err)
+	}
+	fmt.Println("quantos bytes foram copiados", i)
+
+	// Retorna nil se não houver nenhum erro
+	return nil
+}
