34322ac347be127596eff8a9c68ec5598244e32b
Rodrigo Valente <valentergs@gmail.com>
2020-10-08 16:43:22 -0300

    errors.As()

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ import (
 func main() {
 	destino := "arq2.txt"
 	origem := "arq1.txt"
+	var pathErr *os.PathError
 	err := copiar.CopiarArq(destino, origem)
 	if err != nil {
-		// A checagem do tipo erro (errors.Is), geralmente é feito no main(), não na função de origem
-		if errors.Is(err, os.ErrNotExist) {
-			fmt.Printf("Arquivo %s não existe\n", origem)
+		// A checagem do tipo erro (errors.Is ou errors.As), geralmente é feito no main(), não na função de origem
+		if errors.As(err, &pathErr) {
+			fmt.Printf("ARQUIVO: %s, OPERATION: %s, ORIGINAL ERROR: %s\n", pathErr.Path, pathErr.Op, err)
 		}
 		log.Println("erro ao exec copiarArq():", err)
 	}
